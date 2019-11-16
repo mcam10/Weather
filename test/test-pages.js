5422e16336adb429273a9032aa4e9d8eeb08ac3d
mcam1010 <malikcameron95@gmail.com>
2019-11-15 17:03:35 -0800

    Testing Each endpoint in Weather App

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,8 +1,24 @@
 const expect = require('chai').expect;
 const request = require('request');
 
+// testing each endpoint
+
 it('Weather Page Content',function(done){
     request('http://localhost:3000/weather',function(error,response,body){
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+it('About Page Content',function(done){
+    request('http://localhost:3000/about',function(error,response,body){
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+it('Help Page Content',function(done){
+    request('http://localhost:3000/help',function(error,response,body){
         expect(response.statusCode).to.equal(200);
         done();
     });
